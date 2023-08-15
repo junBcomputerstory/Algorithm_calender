7abee8eb78b2ac5f9275598a3cf2c3df0346ae8d
Hongjun <ggaye159@naver.com>
2023-08-15 20:09:16 +0900

    Initial commit

---
+++ src/main/java/Algorithm_Study/Algorithm_calender/AlgorithmCalenderApplication.java
@@ -0,0 +1,13 @@
+package Algorithm_Study.Algorithm_calender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgorithmCalenderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgorithmCalenderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Algorithm_Study/Algorithm_calender/AlgorithmCalenderApplicationTests.java
@@ -0,0 +1,13 @@
+package Algorithm_Study.Algorithm_calender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgorithmCalenderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
